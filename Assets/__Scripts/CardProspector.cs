b4a94d96ee53eb418450134461366608254863cf
kzhang0523 <70768441+kzhang0523@users.noreply.github.com>
2020-11-27 22:55:40 -0500

    CardProspector Script

---
+++ Assets/__Scripts/CardProspector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+	public enum CardState
+	{
+		drawpile,
+		tableau,
+		target,
+		discard
+	}
+	public class CardProspector : Card
+	{ //Make sure CardProspector extends Card
+	  //This is how you use the enum CardState
+		[Header("Set Dynamically: CardProspector")]
+		public CardState state = CardState.drawpile;
+
+		//Thie hiddenBy list stores which other cards will keep this one face down
+		public List<CardProspector> hiddenBy = new List<CardProspector>();
+
+		//LayoutID matches this card to a Layout XML id if it's a tableau card
+		public int layoutID;
+
+		//The SlotDef class stores information pulled in from the LayoutXML <slot>
+		public SlotDef slotDef;
+
+	}
